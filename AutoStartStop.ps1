2bbae5d11f9f0cc49bb45b742a87b550d5acb29b
ciarafreegard2 <115149555+ciarafreegard2@users.noreply.github.com>
2023-02-16 14:35:17 +0000

    made work

--- AutoStartStop.ps1
+++ AutoStartStop.ps1
@@ -1,56 +1,50 @@
+[CmdletBinding()]param (
+[Parameter(Mandatory=$true)]
+[string]$enviroment,
+[Parameter(Mandatory=$true)]
+[string]$operation
+)
 
-try
-{
-    "Logging in to Azure..."
-    Connect-AzAccount -Identity
-}
-catch {
-    Write-Error -Message $_.Exception
-    throw $_.Exception
-}
+Write-Output "Connecting to azure via  Connect-AzAccount -Identity" 
+Connect-AzAccount -Identity 
+Write-Output "Successfully connected with Automation account's Managed Identity" 
 
+$allStartVM = @()
+$allStopVM = @()
 Write-Output "Starting on $(Get-Date)"
 $currenttime = Get-Date -Format HH
-$DevStartTime = 16
-$DevStopTime = 10
-$ProdStartTime = 9
-$ProdStopTime = 16
+# $DevStartTime = 13
+# $DevStopTime = 13
+# $ProdStartTime = 13
+# $ProdStopTime = 13
 
-$var = Get-AzVM -Name * | Where-Object { ($_.Tags['Enviroment'] -eq 'Dev')}
-Write-Output $var
 #Dev
-if((Get-AzVM -Name * | Where-Object { ($_.Tags['Enviroment'] -eq 'Dev')}){  
-    Write-Output "dev"
-    if($DevStartTime -eq $currenttime){ 
-        Write-Output "1"
-        $allStartVM=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($DevStartTime -eq $currenttime))}
-    }
-    if($DevStopTime -eq $currenttime){
-        Write-Output "2"
-        $allStopVM=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($DevStopTime -eq $currenttime))}
-       
-    }
+if(($enviroment -eq "dev") -and ($operation -eq"start")){ 
+    Write-Output "1"
+    $allStartVM+=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($_.Tags['Enviroment'] -eq 'Dev'))}
+}
+if(($enviroment -eq "dev") -and ($operation -eq"stop")){
+    Write-Output "2"
+    $allStopVM+=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($_.Tags['Enviroment'] -eq 'Dev'))}
 }
 
 #Prod
-if((Get-AzVM -Name.Tags.Enviroment)-eq "Prod"){
-    if($ProdStartTime -eq $currenttime){
-        Write-Output "3"
-        $allStartVM=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($ProdStartTime -eq $currenttime))}
-    }
-    if($ProdStopTime -eq $currenttime){
-        Write-Output "4"
-        $allStopVM=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($ProdStopTime -eq $currenttime))}
-    
-    }
+if(($enviroment -eq "prod") -and ($operation -eq"start")){
+    Write-Output "3"
+    $allStartVM+=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($_.Tags['Enviroment'] -eq 'Prod'))}
 }
+if(($enviroment -eq "prod") -and ($operation -eq"stop")){
+    Write-Output "4"
+    $allStopVM+=Get-AzVM -Name * | where {(($_.Tags.AutoStartStop -eq $true) -and ($_.Tags['Enviroment'] -eq 'Prod'))}
+}
+
 
 # Start
 foreach($vm in $allStartVM){
     Write-Output "Starting all VMs that start at $currenttime"
     try{
         $vmname =$vm.Name
-        Write-Output "Starting these vms $vmname"
+        Write-Output "Starting this vm $vmname"
         Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
     }
     catch{
@@ -63,11 +57,10 @@ foreach($vm in $allStopVM){
     Write-Output "Stopping all vms at $currenttime"
     try{
         $vmname =$vm.Name
-        Write-Output "Stopping these vms $vmname"
+        Write-Output "Stopping this vm $vmname"
         Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
     }
     catch{
         Write-Output "Error while executing "
     }
 }
-
